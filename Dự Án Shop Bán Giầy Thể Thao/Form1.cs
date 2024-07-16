45a642be30adefd243694db748c38ba3a4b683ee
admin <admin@DESKTOP-17FI7CO>
2024-07-16 13:28:05 +0700

    Add project files.

---
+++ Dự Án Shop Bán Giầy Thể Thao/Form1.cs
@@ -0,0 +1,10 @@
+namespace Dự_Án_Shop_Bán_Giầy_Thể_Thao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Dự Án Shop Bán Giầy Thể Thao/Program.cs
@@ -0,0 +1,17 @@
+namespace Dự_Án_Shop_Bán_Giầy_Thể_Thao
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
